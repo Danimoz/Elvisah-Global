191930e97cdb11c7fdbc8b515da2ed14879e06b4
Danimoz <azubuinedaniel05@gmail.com>
2024-11-07 11:14:29 +0100

    chore: reset db and integrate live

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,7 +5,7 @@ import SubmitButton from '@/components/submitButton';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Link } from 'lucide-react';
+import Link from 'next/link';
 import { useRef } from 'react';
 import { useFormState } from 'react-dom';
 import { FaWhatsapp, FaInstagram } from 'react-icons/fa';
